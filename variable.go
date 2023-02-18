3f327c0348b2d48c91c018218ad260c2b37665c2
Arafat5682 <arafathossain3066@gmail.com>
2023-02-18 22:48:35 +0600

    second commit

---
+++ variable.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+/* 
+var name string = "Arafat"
+var name2 , city string = "Golang", "Jhenaidah"
+fmt.Println(name , city, name2)
+*/
+
+var c rune ='C'
+var age int = 10
+var result float32 = 30.54
+
+fmt.Println(c, age, result)
+}
