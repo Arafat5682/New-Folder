2782414a565068b9d85a16243388bee3a692c040
Arafat5682 <arafathossain3066@gmail.com>
2023-02-18 23:12:36 +0600

    new

---
+++ new.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Printf("%c\n", 65)
+	fmt.Printf("%c\n", 0x41)
+   
+   fmt.Printf("%x", 'অ')
+
+	 fmt.Printf("%c\n", 0x0985)
+
+      fmt.Printf("%c\n", 2437)
+
+	 //numToChar(65)
+
+    //add(1,3)
+
+
+      
+}
+
+//func add(a string){
+
+//  fmt.Println(a+10)
+
+//}
+
+//func numToChar(num int){
+
+//  fmt.Printf("%c", num)
+
+//}
