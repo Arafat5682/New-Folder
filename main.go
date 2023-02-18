8277140ce16bebe3f07dc3f853a8dfcf4356f65d
Arafat5682 <arafathossain3066@gmail.com>
2023-02-18 22:30:02 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+/* 
+var name string = "Arafat"
+var name2 , city string = "Golang", "Jhenaidah"
+fmt.Println(name , city, name2)
+*/
+
+/*
+var c rune ='C'
+var age int = 10
+var result float32 = 30.54
+
+fmt.Println(c, age, result)
+*/
+
+fmt.Println(true && true)
+  fmt.Println(true && false)
+  fmt.Println(true || true)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+
+var chr rune 
+chr = 'd'
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+
+}
