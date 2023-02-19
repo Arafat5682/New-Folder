b3377942124ea361f807f5bd2172a8ccdd715814
Arafat5682 <arafathossain3066@gmail.com>
2023-02-19 20:32:45 +0600

    String

---
+++ Bangla.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	
+   
+   fmt.Println("অ")
+
+
+      
+}
+
+//func add(a string){
+
+//  fmt.Println(a+10)
+
+//}
+
+//func numToChar(num int){
+
+//  fmt.Printf("%c", num)
+
+//}
---
+++ String.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+   students := [3]string{"Ashiq", "Sabbir", "Shimul"}
+ fmt.Println(students, len(students))
+}
