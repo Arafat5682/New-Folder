fd0167b64a07229418aa631d35d07cfe4a3a71ca
Arafat Hossain <112123673+Arafat5682@users.noreply.github.com>
2023-02-19 23:43:40 +0600

    Add files via upload

---
+++ for.go
@@ -0,0 +1,40 @@
+package main
+
+import(
+    "fmt"
+)
+
+func main(){
+  
+//fmt.Print("Enter your age: ")
+//var age int
+//fmt.Scanf("%d", &age)
+
+//if age < 3 {
+//fmt.Println("Infant")
+
+//}else if age>2 && age<13{
+ 
+//fmt.Println("Children")
+//}else if age>12 && age<=19{
+ //fmt.Println("Teen age")
+//}else{
+  //fmt.Println("Adult")
+//}
+/*
+for i:=1; i<=9; i++ {
+ fmt.Println(i)
+}
+ */
+/*
+students := []string{"Asgor","Mainul","Anonnya"}
+for i, std := range students {
+  fmt.Println(i, std)
+}
+*/
+i :=1
+for i<=10{
+   fmt.Println(i,"Hello")
+ i++
+}
+}
